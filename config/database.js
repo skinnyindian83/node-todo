d39121a66809fc2a64b874158662fc729fc05979
Suhas Prasad <suhas@craniumstack.com>
2017-07-11 18:28:40 -0400

    changed db to EC2

--- config/database.js
+++ config/database.js
@@ -1,4 +1,5 @@
 module.exports = {
-    remoteUrl : 'mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu',
-    localUrl: 'mongodb://localhost/meanstacktutorials'
+    // remoteUrl : 'mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu',
+    remoteUrl: 'mongodb://dummyAdmin:dummyAdmin@ec2-52-11-43-66.us-west-2.compute.amazonaws.com:27017/dummyDB'
+    // localUrl: 'mongodb://localhost/meanstacktutorials'
 };
